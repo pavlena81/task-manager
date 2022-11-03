fdc3fffa7f4d6cfde3e7d741beeb3dfae21ef983
Olena Nesteruk <pavlena81@gmail.com>
2022-11-03 22:38:50 +0200

    add statusFilter

--- src/components/StatusFilter/StatusFilter.js
+++ src/components/StatusFilter/StatusFilter.js
@@ -1,12 +1,20 @@
+import { useSelector } from "react-redux";
+import { statusFilters } from "redux/constants";
+
 import { Button } from "components/Button/Button";
 import css from "./StatusFilter.module.css";
 
+
+
 export const StatusFilter = () => {
+
+  const filter = useSelector(state => state.filters.status);
+
   return (
     <div className={css.wrapper}>
-      <Button>All</Button>
-      <Button>Active</Button>
-      <Button>Completed</Button>
+      <Button selected={filter === statusFilters.all}>All</Button>
+      <Button selected={filter === statusFilters.active}>Active</Button>
+      <Button selected={filter === statusFilters.completed}>Completed</Button>
     </div>
   );
 };
--- src/components/TaskList/TaskList.js
+++ src/components/TaskList/TaskList.js
@@ -1,10 +1,28 @@
 import { Task } from "components/Task/Task";
 import css from "./TaskList.module.css";
+import { useSelector } from "react-redux";
+import { getTasks, getStatusFilter } from "redux/selectors";
+import { statusFilters } from "redux/constants"; 
+
+const getVisibleTasks = (tasks, statusFilter) => {
+  switch (statusFilter) {
+    case statusFilters.active:
+      return tasks.filter(task => !task.completed);
+    case statusFilters.completed:
+      return tasks.filter(task => task.completed);
+    default:
+      return tasks;
+  }
+};
+
 
 export const TaskList = () => {
+  const tasks = useSelector(getTasks);
+  const statusFilter = useSelector(getStatusFilter);
+  const visibleTasks = getVisibleTasks(tasks, statusFilter);
   return (
     <ul className={css.list}>
-      {[].map(task => (
+      {visibleTasks.map(task => (
         <li className={css.listItem} key={task.id}>
           <Task task={task} />
         </li>
